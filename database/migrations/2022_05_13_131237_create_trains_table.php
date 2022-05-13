2e581c06a616f37350a1f49861fb54d84c0ec9f9
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-05-13 15:31:59 +0200

    added trains table to db

--- database/migrations/2022_05_13_131237_create_trains_table.php
+++ database/migrations/2022_05_13_131237_create_trains_table.php
@@ -16,6 +16,15 @@ class CreateTrainsTable extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('azienda', 50);
+            $table->string('Stazione di partenza', 60);
+            $table->string('Stazione di arrivo', 60);
+            $table->time('Orario di partenza');
+            $table->time('Orario di arrivo');
+            $table->string('Codice Treno', 20);
+            $table->tinyInteger('Numero Carrozze');
+            $table->boolean('In orario');
+            $table->boolean('Cancellato');
         });
     }
 
