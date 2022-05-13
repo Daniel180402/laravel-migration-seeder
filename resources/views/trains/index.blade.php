cdbf728063f7fcc4976ad0f40769787bdea82cd6
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-05-13 14:52:01 +0200

    first commit

---
+++ resources/views/trains/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('title', 'Trains')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12 ps-4">
+                <h1>Trains:</h1>
+            </div>
+            <div class="col-4">
+                <div class="card p-3 m-3" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title"></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"></h6>
+                        <p class="card-text"></p>
+                        <p class="card-text"></p>
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
